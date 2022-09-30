c62a28d0e8baf64e597da4c2cfd68f97149dd7d9
Kostya Farber <kostya.farber@gmail.com>
2022-09-30 09:32:16 +0100

    add progress

---
+++ launch-countdown-timer-main/index.js
@@ -0,0 +1,31 @@
+function showTime() {
+
+    var date = new Date();
+    var d = date.getDate();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById('days').innerHTML = d;
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('minutes').innerHTML = m;
+    document.getElementById('seconds').innerHTML = s;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
