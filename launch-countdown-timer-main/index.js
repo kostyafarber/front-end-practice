c2eb529882061fe748eda82944b09c05d00db5d0
Kostya Farber <kostya.farber@gmail.com>
2022-09-30 17:06:00 +0100

    final changes

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2023 12:37:25").getTime();
+var countDownDate = new Date().getTime() + 1209600000 // two weeks in milliseconds;
 
 // Update the count down every 1 second
 var x = setInterval(function() {
