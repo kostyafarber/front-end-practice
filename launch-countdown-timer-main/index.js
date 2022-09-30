249e17390c8c334832e0c9ac52285e5f02575ea0
Kostya Farber <kostya.farber@gmail.com>
2022-09-30 12:37:02 +0100

    finish js on timercountdown

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -1,31 +1,32 @@
-function showTime() {
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2023 12:37:25").getTime();
 
-    var date = new Date();
-    var d = date.getDate();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-    if (h == 0) {
-        h = 12;
-    }
+  // Get today's date and time
+  var now = new Date().getTime();
 
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('days').innerHTML = d;
-    document.getElementById('hours').innerHTML = h;
-    document.getElementById('minutes').innerHTML = m;
-    document.getElementById('seconds').innerHTML = s;
+  // Display the result in the element with id="demo"
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
 
-    setTimeout(showTime, 1000);
-}
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
-showTime();
+setInterval();
