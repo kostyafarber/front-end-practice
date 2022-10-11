96d467c581082ba295548571838dc31d6c360ae5
Kostya Farber <kostya.farber@gmail.com>
2022-10-11 11:37:01 +0100

    fix typo

--- product-preview-card-component-main/src/App.js
+++ product-preview-card-component-main/src/App.js
@@ -3,7 +3,7 @@ import Preview from "./components/Preview";
 function App() {
   const product = {
     productType: "playing card",
-    productName: "Dark Magician Playinf Card @ v1.0",
+    productName: "Dark Magician Playing Card @ v1.0",
     productDesc: `Dark Magician was created when Mahad fused his ba with "Illusion Magician", his ka while fighting Thief King Bakura.`,
     price: "$149.99",
     priceReduced: "$169.99",
